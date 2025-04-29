bab810698b2c9628423c5f452371393dd92bfc4d
Nagarjun Honrao <nagarjun.honrao.5@gmail.com>
2025-04-29 17:17:15 +0530

    Added From User2

---
+++ react-app-new/src/Components/Props/ChildrenProp.jsx
@@ -0,0 +1,13 @@
+function Sard(props) {
+    return (
+        <>
+            {props.children}
+        </>
+    );
+}
+export default Sard;
+
+{/* <Sard>
+    <h2>Hello from Inside Card !</h2>
+    <p>This is Nested Content</p>
+</Sard> */}
